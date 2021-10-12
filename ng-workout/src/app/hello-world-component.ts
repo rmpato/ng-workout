ad7aa0e228ff3f26c223f9699eb8b1500a1c0abd
pato <pato.rodriguez10@gmail.com>
2021-10-12 14:33:01 -0300

    adds basic example of text interpolation

--- ng-workout/src/app/hello-world-component.ts
+++ ng-workout/src/app/hello-world-component.ts
@@ -2,28 +2,32 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
     selector: 'app-hello-world',
-    template: '<h1>{{title}}</h1>',
+    template: '<h1>{{title}}</h1> {{getMin(23,43)}}',
     styles: [`
     h1 {
         color: blue;
     }`]
 })
 
-export class HelloWorldComponent implements OnInit, OnDestroy{
+export class HelloWorldComponent implements OnInit, OnDestroy {
     intervalSub;
     title;
-    
+
     ngOnInit() {
-        this.title = 'Hello World'    
+        this.title = 'Hello World'
         this.intervalSub = setInterval(() => {
             console.log("Hello from ngOnInit")
         }, 1000)
     }
 
     ngOnDestroy() {
-console.log("ondestroy")
-        if(this.intervalSub){
+        console.log("ondestroy")
+        if (this.intervalSub) {
             clearInterval(this.intervalSub);
         }
+    }
+
+    getMin(a, b) {
+        return a < b ? a : b
     }
 }
